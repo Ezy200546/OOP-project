6efc1022e3bcb4304bcd0796a980067e459526f5
Adef Mahamat <adefmahamat656@gmail.com>
2026-02-27 23:03:50 +0600

    Update Cart.h

--- Cart.h
+++ Cart.h
@@ -4,25 +4,14 @@
 #include "Product.h"
 
 class Cart {
+private:
+    Product products[10];
+    int count;
+
 public:
-    Product items[20];
-    int quantities[20];
-    int count = 0;
-
-    void addProduct(Product p, int qty) {
-        items[count] = p;
-        quantities[count] = qty;
-        count++;
-    }
-
-    void viewCart() {
-        cout << "\n--- Cart ---\n";
-        for (int i = 0; i < count; i++) {
-            cout << items[i].name << " x "
-                 << quantities[i]
-                 << " = $" << items[i].price * quantities[i] << endl;
-        }
-    }
+    Cart();
+    void addProduct(Product p);
+    int getCount() const;
 };
 
 #endif

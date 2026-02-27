377fd653dd05e28239e7f5b43c4ece9e1dd3d5b2
Adef Mahamat <adefmahamat656@gmail.com>
2026-02-27 23:04:54 +0600

    Update Order.h

--- Order.h
+++ Order.h
@@ -1,26 +1,15 @@
 #ifndef ORDER_H
 #define ORDER_H
 
-#include <iostream>
-using namespace std;
+#include "Cart.h"
 
 class Order {
+private:
+    Cart cart;
+
 public:
-    int orderId;
-    string buyerName;
-    string status;
-
-    Order(int id, string b) {
-        orderId = id;
-        buyerName = b;
-        status = "Pending";
-    }
-
-    void display() {
-        cout << "Order ID: " << orderId
-             << " | Buyer: " << buyerName
-             << " | Status: " << status << endl;
-    }
+    Order();
 };
 
 #endif
+

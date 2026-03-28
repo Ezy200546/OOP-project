6cc883949f508017a28c9c89bef4d923b37f8b8e
Adef Mahamat <adefmahamat656@gmail.com>
2026-03-28 09:28:33 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstdlib>
 
 #include "User.h"
 #include "Product.h"
 #include "Cart.h"
+#include "Order.h"
 #include "BKashPayment.h"
 #include "CashOnDelivery.h"
 #include "BKashAccounts.h"
 
 using namespace std;
 
-// -------- USER FILE --------
+
 void saveUser(string u, string p)
 {
     ofstream file("users.txt", ios::app);
@@ -32,6 +32,7 @@ bool loginUser(string u, string p)
     }
     return false;
 }
+
 
 int main()
 {
@@ -54,7 +55,7 @@ int main()
 
         if(choice == 0) break;
 
-        // ================= REGISTER =================
+        //REGISTER 
         if(choice == 1)
         {
             string u, p;
@@ -69,21 +70,20 @@ int main()
             cout << "Registration submitted (waiting admin approval)\n";
         }
 
-        // ================= LOGIN =================
-        else if(choice == 2)
+        //  LOGIN
         {
             int type;
             cout << "\n1. Buyer Login\n2. Admin Login\nChoice: ";
             cin >> type;
 
-            // -------- BUYER --------
+            // BUYER 
             if(type == 1)
             {
-                string u,p;
-                cout << "Username: "; cin >> u;
-                cout << "Password: "; cin >> p;
+                string username, password;
+                cout << "Username: "; cin >> username;
+                cout << "Password: "; cin >> password;
 
-                if(loginUser(u,p))
+                if(loginUser(username,password))
                 {
                     cout << "Login successful!\n";
 
@@ -102,13 +102,13 @@ int main()
 
                         if(bchoice == 0) break;
 
-                        // VIEW PRODUCTS
+                        // -------- VIEW PRODUCTS --------
                         if(bchoice == 1)
                         {
                             Product::listAllProducts();
                         }
 
-                        // ADD TO CART
+                        // -------- ADD TO CART --------
                         else if(bchoice == 2)
                         {
                             int id, qty;
@@ -117,22 +117,47 @@ int main()
                             cout << "Quantity: ";
                             cin >> qty;
 
-                            // NOTE: simple product object
-                            Product p(id,"Temp","Temp",100,10);
-                            cart.addItem(p, qty);
+                            Product p;
+
+                            if(Product::getProductById(id, p))
+                            {
+                                if(qty <= p.getStock())
+                                {
+                                    cart.addItem(p, qty);
+                                }
+                                else
+                                {
+                                    cout << "Not enough stock!\n";
+                                }
+                            }
+                            else
+                            {
+                                cout << "Product not found!\n";
+                            }
                         }
 
-                        // VIEW CART
+                        // VIEW CART 
                         else if(bchoice == 3)
                         {
                             cart.viewCart();
                         }
 
-                        // CHECKOUT
+                        //  CHECKOUT + ORDER 
                         else if(bchoice == 4)
                         {
                             double total = cart.getTotal();
-                            cout << "Total: " << total << endl;
+
+                            if(total == 0)
+                            {
+                                cout << "Cart is empty!\n";
+                                continue;
+                            }
+
+                            cout << "Total: " << total << " Taka\n";
+
+                           
+                            Order order(username);
+                            order.createOrder(cart);
 
                             int pay;
                             cout << "1. bKash\n2. Cash On Delivery\nChoice: ";
@@ -147,18 +172,27 @@ int main()
                                 BKashAccounts acc;
                                 acc.loadAccounts("bKashAccounts.txt");
 
-                                BKashPayment payment(total, number);
-
-                                if(payment.validate())
-                                    payment.process(acc);
+                                order.setPaymentMethod(new BKashPayment(total, number));
                             }
                             else
                             {
-                                CashOnDelivery payment(total);
-                                BKashAccounts dummy;
-                                payment.process(dummy);
+                                order.setPaymentMethod(new CashOnDelivery(total));
                             }
 
+                            cout << "Order placed successfully!\n";
+
+                            
+                            for(int i = 0; i < cart.getCount(); i++)
+                            {
+                                Product p = cart.getProduct(i);
+                                int qty = cart.getQuantity(i);
+
+                                int newStock = p.getStock() - qty;
+
+                                Product::updateStock(p.getId(), newStock);
+                            }
+
+                            
                             cart.clearCart();
                         }
                     }
@@ -169,7 +203,7 @@ int main()
                 }
             }
 
-            // -------- ADMIN --------
+            // ADMIN 
             else if(type == 2)
             {
                 string pass;
@@ -189,6 +223,7 @@ int main()
                         cout << "2. Add Product\n";
                         cout << "3. Delete Product\n";
                         cout << "4. View Products\n";
+                        cout << "5. View Orders\n";
                         cout << "0. Logout\n";
                         cout << "Choice: ";
                         cin >> achoice;
@@ -199,9 +234,7 @@ int main()
                         if(achoice == 1)
                         {
                             if(pendingCount == 0)
-                            {
                                 cout << "No pending users\n";
-                            }
 
                             for(int i=0;i<pendingCount;i++)
                             {
@@ -219,7 +252,7 @@ int main()
                             }
                         }
 
-                        // ADD PRODUCT
+                        // ADD PRODUCT 
                         else if(achoice == 2)
                         {
                             int id, stock;
@@ -238,20 +271,27 @@ int main()
                             cout << "Product added successfully!\n";
                         }
 
-                        // DELETE PRODUCT2
+                        //DELETE PRODUCT 
                         else if(achoice == 3)
                         {
                             int id;
-                            cout << "Enter product ID to delete: ";
+                            cout << "Enter product ID: ";
                             cin >> id;
 
                             Product::deleteProduct(id);
                         }
 
-                        // VIEW PRODUCTS
+                        // VIEW PRODUCTS 
                         else if(achoice == 4)
                         {
                             Product::listAllProducts();
+                        }
+
+                        //VIEW ORDERS
+                        else if(achoice == 5)
+                        {
+                            Order o("admin"); 
+                            o.listOrders();
                         }
                     }
                 }
